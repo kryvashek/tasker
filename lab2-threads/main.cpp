03dd779235f195708af14097ff8c732e1357fa0b
Kryvashek <kryvashek@gmail.com>
2016-03-16 20:36:23 +0200

    Removed unneeded operator, moved other to main, changed makefile to build it.
    Signed-off-by: Kryvashek <kryvashek@gmail.com>

--- lab2-threads/main.cpp
+++ lab2-threads/main.cpp
@@ -1,12 +1,23 @@
 #include <fstream>
-#include "kit.hpp"
 #include "threadarmy.hpp"
 
 using std::endl;
 using std::cout;
 using std::fstream;
+using std::istream;
 
-typedef float			ItemT;
+// gets one value of Kit entry type and puts it to the end of given Kit
+template< class StorageT >
+istream & operator>>( istream & input, StorageT & destination )
+	{
+	static typename StorageT::value_type	value;
+
+	input >> value;
+	destination.push_back( value );
+	return input;
+	}
+
+typedef int				ItemT;
 typedef vector< ItemT >	KitT;
 
 int main( void )
@@ -25,6 +36,8 @@ int main( void )
 		input >> cortege2;
 
 	army.Execute( cortege1, cortege2, n );
+	cout << army.Stop() << endl;
+	army.Execute( cortege1, cortege2, n - 1 );
 	cout << army.Stop() << endl;
 	return 0;
 	}
