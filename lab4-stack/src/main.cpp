fbbabfdd840d93388f6e729facec3f18a0a00cf4
kryvashek <kryvashek@gmail.com>
2016-11-17 21:12:30 +0200

    Rewritten main() to test all.
    Signed-off-by: kryvashek <kryvashek@gmail.com>

--- lab4-stack/src/main.cpp
+++ lab4-stack/src/main.cpp
@@ -56,75 +56,27 @@ int pop( istream & input, string & message, SafeStack< int > & locker ) {
 }
 
 int main( int argc, char *argv[] ) {
-	const __time_t	allStart( getMoment() );
-	const int		count( 8 );
-	int 			index;
-
-	ThreadGuard< char, __time_t >	guard( someFunc );
-	ThreadSquad						squad( 2 );
-
-	guard.Run( '0', allStart );
-
-	for( index = 0; index < count; index++ )
-		squad.AddTask( [ index, allStart ]( void ) {
-			timespec period{ .tv_sec = ( 5 + ( index % 2 ) * 3 ), .tv_nsec = 0 };
-
-			cout << index + 1 << " started: " << getMoment() - allStart << endl;
-			nanosleep( &period, NULL );
-			cout << index + 1 << " finished: " << getMoment() - allStart << endl;
-		} );
-	squad.Run();
-	squad.StopWait();
-
-	guard.StopWait();
-	guard.Set( []( char a, __time_t value ) {
-		timespec period{ .tv_sec = 3, .tv_nsec = 0 };
-
-		cout << a << " started: " << getMoment() - value << endl;
-		nanosleep( &period, NULL );
-		cout << a << " finished: " << getMoment() - value << endl;
-	} );
-	guard.Run( '9', allStart );
-	guard.StopWait();
-
-	cout << "All got: " << getMoment() - allStart << endl;
-
-	Executor< int >	executor;
-
-	executor.AddRoutine( "write", write );
-	executor.Run();
-
-	SafeStack< int >	locker;
-
-	for( int i = 0; i < 10; i++ ) {
-		cout << ( 2 * i + 1 ) << " ";
-		locker.push( 2 * i + 1 );
-	}
-
-	cout << endl;
-
-	while( !locker.empty() ) {
-		cout << locker.top() << " ";
-		locker.pop();
-	}
-
-	cout << endl;
-
 	ifstream	inone( "first.txt" ),
 				intwo( "second.txt" );
 	ofstream	outone( "first.log"),
 				outtwo( "second.log" );
 
-	Executor< int, SafeStack< int > & >	one( inone, outone ),
-										two( intwo, outtwo );
+	ThreadSquad			squad( 2 );
+	SafeStack< int >	locker;
+
+	Executor< int, SafeStack< int > & >	one( inone, cout ),
+										two( intwo, cout );
 
 	one.AddRoutine( "top", top );
 	one.AddRoutine( "push", push );
 	one.AddRoutine( "pop", pop );
 	two = one; // copying of the instruction set
 
-	one.Run( locker );
-	two.Run( locker );
+	squad.AddTask( [ & ]( void ) { one.Run( locker ); } );
+	squad.AddTask( [ & ]( void ) { two.Run( locker ); } );
+
+	squad.Run();
+	squad.StopWait();
 
 	return 0;
 }
